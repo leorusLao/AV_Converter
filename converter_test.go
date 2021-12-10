d7d5d6b98bee1ca324c142fa547fc3595af01aa4
leorusLao <495325806@qq.com>
2021-12-10 20:10:36 +0800

    add test

--- converter_test.go
+++ converter_test.go
@@ -4,10 +4,11 @@ import (
 	"github.com/leorusLao/AV_Converter/common"
 	"log"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
-var (
+const (
 	audio  = "./testdata/audio/"
 	video  = "./testdata/video/"
 	output = "./output/"
@@ -27,52 +28,60 @@ func fileExist(path string) bool {
 	return !os.IsNotExist(err)
 }
 
+type testData struct {
+	in   string
+	outs []string
+}
+
+func getData(in string) testData {
+	format := filepath.Ext(in)[1:]
+	return testData{
+		in: in,
+		outs: []string{
+			output + format + ".wav",
+			output + format + ".mp3",
+			output + format + ".flac",
+			output + format + ".ogg",
+		}}
+}
+
 func TestConvert(t *testing.T) {
-	tests := []struct {
-		in   string
-		outs []string
-	}{
-		// wav
-		{
-			in: audio + "16K.wav",
-			outs: []string{
-				output + "wav.wav",
-				output + "wav.mp3",
-				output + "wav.flac",
-				output + "wav.ogg",
-			},
-		},
-		// aac
-		{
-			in: audio + "aac.aac",
-			outs: []string{
-				output + "aac.wav",
-				output + "aac.mp3",
-				output + "aac.flac",
-				output + "aac.ogg",
-			},
-		},
-		// ac3
-		{
-			in: audio + "ac3.ac3",
-			outs: []string{
-				output + "ac3.wav",
-				output + "ac3.mp3",
-				output + "ac3.flac",
-				output + "ac3.ogg",
-			},
-		},
-		// adu
-		{
-			in: audio + "adu.adu",
-			outs: []string{
-				output + "adu.wav",
-				output + "adu.mp3",
-				output + "adu.flac",
-				output + "adu.ogg",
-			},
-		},
-		// 其它略
+	tests := []testData{
+		// audio
+		getData(audio + "16K.wav"),
+		getData(audio + "aac.aac"),
+		getData(audio + "ac3.ac3"),
+		getData(audio + "adu.adu"),
+		getData(audio + "aiff.aiff"),
+		getData(audio + "amr.amr"),
+		getData(audio + "ape.ape"),
+		getData(audio + "aud.aud"),
+		getData(audio + "backup.backup"),
+		getData(audio + "flac.flac"),
+		getData(audio + "hzmv.hzmv"),
+		getData(audio + "m4a.m4a"),
+		getData(audio + "mp2.mp2"),
+		getData(audio + "mp3.mp3"),
+		getData(audio + "pcm.pcm"),
+		getData(audio + "silk.silk"),
+		getData(audio + "slk.slk"),
+		getData(audio + "spx.spx"),
+		getData(audio + "vox.vox"),
+		getData(audio + "wavf.wavf"),
+		getData(audio + "wma.wma"),
+		// video
+		getData(video + "3gp.3gp"),
+		getData(video + "asf.asf"),
+		getData(video + "avi.avi"),
+		getData(video + "flv.flv"),
+		getData(video + "m4v.m4v"),
+		getData(video + "mkv.mkv"),
+		getData(video + "mov.mov"),
+		getData(video + "mp4.mp4"),
+		getData(video + "mpg.mpg"),
+		getData(video + "swf.swf"),
+		getData(video + "vob.vob"),
+		getData(video + "wmv.wmv"),
 	}
 
 	for _, v := range tests {
